226b61a50041399b187303c3cad24f35fc29da7d
Reshika Shrestha <shresthareshika@gmail.com>
2021-09-23 13:07:48 +0545

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBBYyuuOWehHKcuY8SnXwV1SSKoLV2p0Dk",
+    authDomain: "uchat-62b8b.firebaseapp.com",
+    projectId: "uchat-62b8b",
+    storageBucket: "uchat-62b8b.appspot.com",
+    messagingSenderId: "829187379570",
+    appId: "1:829187379570:web:1b135b5fac3755d70e4deb",
+    measurementId: "G-S7EDG5V99G"
+  };
