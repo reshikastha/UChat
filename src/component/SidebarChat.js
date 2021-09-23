8df5c31b312e4b5f8393b54cb8690b0fe7dc24d5
Reshika Shrestha <shresthareshika@gmail.com>
2021-09-23 19:56:04 +0545

    sidebar design

---
+++ src/component/SidebarChat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/SidebarChat.css'
+
+function SidebarChat() {
+    return (
+        <div className="sidebarChat">
+            
+        </div>
+    )
+}
+
+export default SidebarChat
