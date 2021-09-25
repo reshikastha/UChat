391984cc8863ea6f7ba40a2f8c90be74a5bdf1a5
Reshika Shrestha <shresthareshika@gmail.com>
2021-09-25 19:46:10 +0545

    firebase error solved

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,8 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 const firebaseConfig = {
   apiKey: "AIzaSyBBYyuuOWehHKcuY8SnXwV1SSKoLV2p0Dk",
   authDomain: "uchat-62b8b.firebaseapp.com",
@@ -9,9 +12,12 @@ const firebaseConfig = {
   appId: "1:829187379570:web:feadaa292a01b7380e4deb",
   measurementId: "G-XVY998LG91"
 };
+// const firebaseApp = initializeApp(firebaseConfig);
+// const db = firestore(firebaseApp)
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider= new firebase.auth.GoogleAuthProvider();
 export {auth, provider};
 export default db;
+
